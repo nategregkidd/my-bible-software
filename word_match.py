bea1e12cf5230be6d7ae83640a9af8d40ca129d5
nategregkidd <54556101+nategregkidd@users.noreply.github.com>
2022-11-27 01:41:56 +0000

    Made basic bible book querying system.

--- word_match.py
+++ word_match.py
@@ -1,1 +1,22 @@
 # This is a test program to attempt to simulate a concordance.
+import pandas as pd
+
+bibledata = pd.read_csv("./csv/t_asv.csv")
+print(bibledata)
+print()
+print(bibledata.head())
+print()
+print(bibledata.columns)
+print()
+ids = bibledata["id"]
+print(ids)
+
+print("\n\n\n")
+while True:
+    book = input("which book ya want?: ")
+    requested_book = bibledata[bibledata['b'] == int(book)]
+    print(requested_book.head())
+
+genesis = bibledata[bibledata['c'] == 1]
+print("\n\n\n genesis!")
+print(genesis.head())
